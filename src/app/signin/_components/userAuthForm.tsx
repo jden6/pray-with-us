4a6626be46f9f0487b0b349514b6dbf9d91231e4
jden6 <jykim6.cloud@gmail.com>
2023-11-16 12:48:26 +0900

    fixed warning code

--- src/app/signin/_components/userAuthForm.tsx
+++ src/app/signin/_components/userAuthForm.tsx
@@ -6,24 +6,22 @@ import { cn } from '@/lib/utils'
 import { Icons } from '@/components/icons'
 import { Button } from '@/components/ui/button'
 import { signIn } from 'next-auth/react'
-import { usePathname, useRouter } from 'next/navigation'
 // import { Input } from '@/components/ui/input'
 // import { Label } from '@/components/ui/label'
 
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 export function UserAuthForm ({ className, ...props }: UserAuthFormProps) {
-  const location = usePathname()
   const [isLoading, setIsLoading] = React.useState<boolean>(false)
 
-  async function onSubmit (event: React.SyntheticEvent) {
-    event.preventDefault()
-    setIsLoading(true)
-
-    setTimeout(() => {
-      setIsLoading(false)
-    }, 3000)
-  }
+  // async function onSubmit (event: React.SyntheticEvent) {
+  //   event.preventDefault()
+  //   setIsLoading(true)
+  //
+  //   setTimeout(() => {
+  //     setIsLoading(false)
+  //   }, 3000)
+  // }
 
   return (
     <div className={cn('grid gap-6', className)} {...props}>
