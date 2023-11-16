37a3ab08e845547713913e8f16483978af21ec9f
jden6 <jykim6.cloud@gmail.com>
2023-11-16 13:01:32 +0900

    PRAY-26 OAuth2 Google 추가

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,18 @@
 import NextAuth, { type AuthOptions } from 'next-auth'
 
 import NaverProvider from 'next-auth/providers/naver'
+import GoogleProvider from 'next-auth/providers/google'
 
 const authOption: AuthOptions = {
   providers: [
     NaverProvider({
       clientId: process.env.NAVER_CLIENT_ID ?? '',
       clientSecret: process.env.NAVER_CLIENT_SECRET ?? ''
-    })
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ''
+    }),
   ],
   pages: {
     signIn: '/signin',
