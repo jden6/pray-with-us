5a3580739a27877aad57a3bcadbb77ea7f4700fb
jden6 <jykim6.cloud@gmail.com>
2023-12-17 19:52:03 +0900

    PRAY-29, PRAY-30 기도 수정 기능 일부 수정
    * 생성 일자가 무조건 오늘로 보이는 문제 수정
    * 수정시 UI가 기존 내용으로 보였다가 리스트페이지로 이동하는 버그 수정
    * 수정 시, 업데이트 일자 적용되도록 수정
    * 수정이 되지 않는 현상 수정

--- src/server/routers/pray.router.ts
+++ src/server/routers/pray.router.ts
@@ -30,7 +30,7 @@ export const prayRouter = router({
             name,
             group_seq
           )
-        `
+        `,
         )
         .match({ "t_users.group_seq": ctx.user.group_seq })
         .order("created_at", { ascending: false })
@@ -81,7 +81,7 @@ export const prayRouter = router({
         t_users (
           group_seq
         )
-      `
+      `,
       )
       .filter("t_users.group_seq", "eq", group_seq)
       .match({ group_seq });
@@ -97,8 +97,8 @@ export const prayRouter = router({
   update: publicProcedure
     .input(TPrayUpdateSchema)
     .mutation(async ({ input }) => {
-      const { pray_seq, ...rest } = input;
-      return supa().from("t_pray").update(rest).eq("pray_seq", pray_seq);
+      const { pray_seq, content } = input;
+      return supa().from("t_pray").update({ content }).eq("pray_seq", pray_seq);
     }),
   delete: publicProcedure
     .input(praySchema.shape.pray_seq)
